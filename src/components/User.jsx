26081e24629b93d844e4258e3060f4e9c439900b
Umesh Manandhar <mandr.umesh@gmail.com>
2024-11-08 14:55:03 +0545

    minor changes

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 
 const User = () => {
-    const {userId, userName} = useParams();
+  const { userId, userName } = useParams();
 
   return (
-    <div className='container'>
-        <h4>User Profile</h4>
-        <h5>User Id: { userId }</h5>
-        <h5>User Name: { userName }</h5>
+    <div className='container mt-5'>
+      <h4>User Profile</h4>
+      <ul className='list-unstyled'>
+        <li>User Id: {userId}</li>
+        <li> User Name: {userName}</li>
+      </ul>
     </div>
   )
 }
